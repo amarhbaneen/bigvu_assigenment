08bc7c6482c195201be2e133743733d0bf272c19
amara <amaralzana@gmail.com>
2021-07-31 05:15:02 +0300

    added two fucntion for canvas.component.ts 1) writeOnCanvas : a function to draw text of user input on the canvas 2) imageOnCanvas : a fucntion to draw image of user choose from dropdownmenu add a new class "people.model.ts " that include name and value << to import the JSON file >> change the html file to contain listener for event like input of user and select change by NgModel and NgModelChange and Input method by angular added ngfor for user choose from importing JSON to People array
    download the JSON file from the link "https://bigvu-interviews-assets.s3.amazonaws.com/presenters.json" since the server doesnt allow get request
    // will be fixed later

---
+++ src/app/canvas/people.model.ts
@@ -0,0 +1,13 @@
+
+export class people {
+
+  public name: string;
+  public value: string;
+
+  constructor(name: string, value: string) {
+    this.name = name;
+    this.value = value;
+
+  }
+}
+
